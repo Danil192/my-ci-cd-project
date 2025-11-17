62ebc646b909814cfcb68b75fde295be6ecf5362
Даниил Русанов <danil192193@mail.ru>
2025-11-17 23:42:14 +0800

    обновлены тесты

--- test_app.py
+++ test_app.py
@@ -1,4 +1,10 @@
 from app import add
 
-def test_add():
-    assert add(2, 3) == 5
+def test_add_negative():
+    assert add(-5, -3) == -8
+
+def test_add_zero():
+    assert add(0, 10) == 10
+
+def test_add_big_numbers():
+    assert add(1000000, 2000000) == 3000000
